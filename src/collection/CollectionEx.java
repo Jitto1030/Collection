1021cad74cfd8aebc7bbdcf94c85fd29c371150a
Jitto1030 <jittowork1030@gmail.com>
2025-08-01 13:19:59 +0530

    Added Some methods in CollectionEx.

--- src/collection/CollectionEx.java
+++ src/collection/CollectionEx.java
@@ -2,6 +2,7 @@ package collection;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Iterator;
 
 public class CollectionEx {
     public static void main(String[] args) {
@@ -11,9 +12,22 @@ public class CollectionEx {
         names.add("Jerico");
         names.add("Piccaso");
         names.add("Anishtone");
+        System.out.println("Names :"+names);
 
-        for (String name : names){
-            System.out.println(name);
+        Collection<String> extraNames = new ArrayList<>();
+        extraNames.add("jenish");
+        extraNames.add("Jensiya");
+        extraNames.add("romario");
+
+        names.addAll(extraNames);
+        System.out.println("Names after add all "+names);
+
+        boolean isContain = names.contains("Jerico");
+        System.out.println("Is the name contains "+isContain);
+
+        Iterator<String> it = names.iterator();
+        while (it.hasNext()){
+            System.out.println(it.next());
         }
     }
 }
